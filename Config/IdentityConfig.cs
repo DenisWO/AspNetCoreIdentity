47cd895572b6cf86956c8b6d3c32c72f5433144b
DenisWO96 <o.denis96@gmail.com>
2021-07-13 19:35:10 -0300

    Adding modifications about Startup class

---
+++ Config/IdentityConfig.cs
@@ -0,0 +1,37 @@
+﻿using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddAuthorizationConfig(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Delete", policy => policy.RequireClaim("Delete"));
+
+                options.AddPolicy("Read", policy => policy.Requirements.Add(new PermissionRequired("Read")));
+                options.AddPolicy("Write", policy => policy.Requirements.Add(new PermissionRequired("Write")));
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+            return services;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,14 @@
 using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Config;
 using AspNetCoreIdentity.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreIdentity
 {
@@ -25,43 +21,24 @@ namespace AspNetCoreIdentity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AspNetCoreIdentityContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI()
-                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("Delete", policy => policy.RequireClaim("Delete"));
-
-                options.AddPolicy("Read", policy => policy.Requirements.Add(new PermissionRequired("Read")));
-                options.AddPolicy("Write", policy => policy.Requirements.Add(new PermissionRequired("Write")));
-            });
-
-            services.AddSingleton<IAuthorizationHandler, PermissionRequiredHandler>();
+            services.AddIdentityConfig(Configuration);
+            services.AddAuthorizationConfig();
+            services.ResolveDependencies();
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
